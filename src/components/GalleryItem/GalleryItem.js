b2065c8d66bbbca479bf8d4dba8aefd1ee6569dc
laramaurine <laramaurine@gmail.com>
2020-12-05 21:41:30 -0600

    cleaning up code

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -12,17 +12,17 @@ class GalleryItem extends Component {
         };
         
         likePic = () => (event) => {
-        axios({
-        method: 'PUT',
-        url: 'gallery/like/' + this.props.pic.id,
+            axios({
+                method: 'PUT',
+                url: 'gallery/like/' + this.props.pic.id,
         })
         .then((response) => {
        
-        this.props.getGallery();
+            this.props.getGallery();
         })
         .catch((error) => {
-        console.log('error in itemjs like', error);
-        alert('error in likes no one likes');
+            console.log('error in itemjs like', error);
+            alert('error in likes no one likes');
         });
         }
     
@@ -34,9 +34,9 @@ class GalleryItem extends Component {
             : <div onClick={this.showDescription}><img src={this.props.pic.path}/></div>}
 
             <div className="divdisplay">
-            {/*<div onClick={this.showDescription}><img src={this.props.pic.path}/></div>*/}
+            
             <button onClick={this.likePic(this.props.pic.id)}>Like</button>
-            {/*<button onClick={this.likePic('1')}>Like</button>*/}
+            
             <p>{this.props.pic.likes} likes</p>
             
             </div>
