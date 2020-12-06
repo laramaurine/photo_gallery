c59c99e1bc4af13720173900efa26244bfcbb295
laramaurine <laramaurine@gmail.com>
2020-12-05 19:43:44 -0600

    pics added

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0,
+      id: 2, path: 'images/burnedcookie.jpg', description: 'I decorated a cookie to resemble my burns', likes: 0,
+      id: 3, path: 'images/ghost.jpg', description: 'I feel like this came from a haunted creamery', likes: 0,
+
+
+
+    }
 ];
 
 module.exports = galleryItems;
---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class GalleryList extends Component {
+
+    render(){
+        return(
+     
+        <>
+        <p>Hello from GalleryList</p>
+        </>
+        )
+    }
+}
+
+export default GalleryList;
